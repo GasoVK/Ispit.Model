608fc07b295dc807bc53e9940d72c264bde082d2
GasoVK <gasparevicivan80@gmail.com>
2026-04-20 20:53:02 +0200

    dodano

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using Ispit.Model;
+using Ispit.Model1; // Ključna linija koja povezuje projekte
 
-namespace Ispit.Konzola
+namespace Ispit.Model
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
@@ -13,34 +13,32 @@ namespace Ispit.Konzola
             for (int i = 1; i <= 3; i++)
             {
                 Console.WriteLine($"--- Unos podataka za {i}. učenika ---");
-                Ucenik noviUcenik = new Ucenik();
+                Ucenik u = new Ucenik();
 
                 Console.Write("Ime: ");
-                noviUcenik.Ime = Console.ReadLine();
+                u.Ime = Console.ReadLine();
 
                 Console.Write("Prezime: ");
-                noviUcenik.Prezime = Console.ReadLine();
+                u.Prezime = Console.ReadLine();
 
-                Console.Write("Datum rođenja (yyyy-mm-dd): ");
-                noviUcenik.DatumRodjenja = DateTime.Parse(Console.ReadLine());
+                Console.Write("Datum rođenja (npr. 2005-05-15): ");
+                u.DatumRodjenja = DateTime.Parse(Console.ReadLine());
 
                 Console.Write("Prosjek (npr. 4,5): ");
-                noviUcenik.Prosjek = double.Parse(Console.ReadLine());
+                u.Prosjek = double.Parse(Console.ReadLine());
 
-                ucenici.Add(noviUcenik);
+                ucenici.Add(u);
                 Console.WriteLine();
             }
 
-            Console.WriteLine("\n================ ISPIS UČENIKA ================");
+            Console.WriteLine("\n======= REZULTATI =======");
             foreach (var u in ucenici)
             {
                 Console.WriteLine($"Učenik: {u.Ime} {u.Prezime}");
-                Console.WriteLine($"Starost: {u.Starost()} godina");
-                Console.WriteLine($"Prosjek: {u.Prosjek} ({u.IspisProsjeka()})");
-                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine($"Starost: {u.Starost()} god.");
+                Console.WriteLine($"Uspjeh: {u.IspisProsjeka()}");
+                Console.WriteLine("-------------------------");
             }
-
-            Console.WriteLine("Pritisnite bilo koju tipku za kraj...");
             Console.ReadKey();
         }
     }
