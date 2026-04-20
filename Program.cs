8764df3479283120383fe9e26c5c9d0fbdac9d52
GasoVK <gasparevicivan80@gmail.com>
2026-04-20 20:27:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Ispit.Model;
+
+namespace Ispit.Konzola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ucenik> ucenici = new List<Ucenik>();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"--- Unos podataka za {i}. učenika ---");
+                Ucenik noviUcenik = new Ucenik();
+
+                Console.Write("Ime: ");
+                noviUcenik.Ime = Console.ReadLine();
+
+                Console.Write("Prezime: ");
+                noviUcenik.Prezime = Console.ReadLine();
+
+                Console.Write("Datum rođenja (yyyy-mm-dd): ");
+                noviUcenik.DatumRodjenja = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Prosjek (npr. 4,5): ");
+                noviUcenik.Prosjek = double.Parse(Console.ReadLine());
+
+                ucenici.Add(noviUcenik);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n================ ISPIS UČENIKA ================");
+            foreach (var u in ucenici)
+            {
+                Console.WriteLine($"Učenik: {u.Ime} {u.Prezime}");
+                Console.WriteLine($"Starost: {u.Starost()} godina");
+                Console.WriteLine($"Prosjek: {u.Prosjek} ({u.IspisProsjeka()})");
+                Console.WriteLine("-----------------------------------------------");
+            }
+
+            Console.WriteLine("Pritisnite bilo koju tipku za kraj...");
+            Console.ReadKey();
+        }
+    }
+}
